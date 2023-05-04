5da5d46f047f751027b27040af34669872ec6e01
callo <56754656+bool-Callo@users.noreply.github.com>
2023-05-04 22:46:22 +0200

    gogogo

--- hodoIevendothis/Program.cs
+++ hodoIevendothis/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections;
+using System.Threading.Channels;
 
 
 //task1
@@ -36,12 +37,77 @@ AnaL.Add("Gacha");
 //loop 
 foreach(var fallus in AnaL)
 {
-    Console.WriteLine(fallus + "\n");
+    Console.WriteLine(fallus);
 }
 
-//task3 alternative
+//task3 alternative - List
 Console.WriteLine("\n" +
-    "task3.1");
+    "task3.1 list edition");
+    
+    List<string> channels = new List<string>();
+    channels.Add("Weebshit");
+    channels.Add("General");
+    channels.Add("Memes");
+    channels.Add("Gacha");
+    channels.Add("Vidya");
+//trying different kind of loop
+int count1 = 0;
+while (count1 < channels.Count())
+    {
+        Console.WriteLine(count1 + ". " + channels[count1]);
+
+            count1++;
+    }
+//task4 using the list from 3.1 
+Console.WriteLine("\n" +
+    "task4\n");
+
+count1 = 0;
+while(count1 < channels.Count())
+{
+    if(count1 % 2 == 0)
+    {
+        Console.WriteLine(count1 + ". " + channels[count1]);
+
+        
+    }
+    count1++;
+}
+//task5
+Console.WriteLine("\n" +
+    "task5\n");
+
+
+static int FibonCiag(int fib1) 
+    {
+        int countfib = 1;
+        fib1 = 0;
+        int fib2 = 1;
+        while (countfib <= 5)
+        {
+            Console.WriteLine(fib1);
+            fib1 = fib1 + fib2;
+            Console.WriteLine(fib2);
+            fib2 = fib2 + fib1;
+            countfib++;
+            
+        }
+
+        return fib1;
+    }
+int result1 = 0;
+FibonCiag(result1);
+   
+
+
+
+
+
+
+
+
+
+
 
 
 
